a76ee845170e0b5286e90b30fd8af176a53a69b5
hoyong <dnguq777@naver.com>
2024-03-14 14:01:44 +0900

    docs : board 폴더 내의 클래스 및 board-mapper 추가
    docs : board 폴더 내의 클래스 및 board-mapper 추가
    author : hoyong
    
    board 폴더 내의 Service, Dao, Board 클래스 생성 및 db폴더내의 board-mapper.xml파일 생성

---
+++ src/main/java/com/fp/board/model/service/BoardService.java
@@ -0,0 +1,10 @@
+package com.fp.board.model.service;
+
+import com.fp.board.model.dao.BoardDao;
+import static com.fp.common.template.JDBCTemplate.*;
+
+public class BoardService {
+	
+	private BoardDao bDao = new BoardDao();
+
+}
