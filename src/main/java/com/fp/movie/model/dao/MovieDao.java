49c9f23fb414b0cdb2a490053bf48d16aa99b950
leeyonghum <yonghun92@naver.com>
2024-03-14 14:30:15 +0900

    MovieService, Dao.java파일 생성
    파일만 생성해놨습니다

---
+++ src/main/java/com/fp/movie/model/dao/MovieDao.java
@@ -0,0 +1,5 @@
+package com.fp.movie.model.dao;
+
+public class MovieDao {
+
+}
---
+++ src/main/java/com/fp/movie/model/service/MovieService.java
@@ -0,0 +1,9 @@
+package com.fp.movie.model.service;
+
+import com.fp.movie.model.dao.MovieDao;
+
+public class MovieService {
+	
+	private MovieDao mDao = new MovieDao();
+	
+}
