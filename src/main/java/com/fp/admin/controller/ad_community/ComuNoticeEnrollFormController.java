a113aae239fb787b16e39b6e4378486206687eb4
suminghaan <sm3597@daum.net>
2024-03-18 14:15:02 +0900

    add_sumin
    add admin_공지사항 등록양식 파일 이름 수정

--- src/main/java/com/fp/admin/controller/ad_community/ComuNoticeEnrollFormController.java
+++ src/main/java/com/fp/admin/controller/ad_community/ComuNoticeEnrollFormController.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class noticeEnrollFormController
  */
 @WebServlet("/enrollForm.co")
-public class noticeEnrollFormController extends HttpServlet {
+public class ComuNoticeEnrollFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public noticeEnrollFormController() {
+    public ComuNoticeEnrollFormController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -26,8 +26,8 @@ public class noticeEnrollFormController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		request.getRequestDispatcher("/views/admin/ad_community/commuNoticeEnroll.jsp").forward(request, response);
 	}
 
 	/**
