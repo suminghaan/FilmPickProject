02d9e15c7f85577148a42ed80de9c0038e21e834
leeyonghum <yonghun92@naver.com>
2024-03-18 17:22:03 +0900

    서치필터 수정 [용훈]
    수정

--- src/main/java/com/fp/movie/model/vo/SearchFilter.java
+++ src/main/java/com/fp/movie/model/vo/SearchFilter.java
@@ -12,99 +12,90 @@ public class SearchFilter {
 	private String orderBy; // 필터
 	
 	
-	
-	
-	private int likeCount; // 인기순
-	private String mvName; // 가나다순
-	private int rewiewCount; // 리뷰순
-	
 	public SearchFilter() {
 		
 	}
 
+
 	public SearchFilter(String currentScreening, String categoryName, String viewRatiog, String mvOpenDate,
-			String mvNation, int likeCount, String mvName, int rewiewCount) {
+			String mvNation, String orderBy) {
 		super();
 		this.currentScreening = currentScreening;
 		this.categoryName = categoryName;
 		this.viewRatiog = viewRatiog;
 		this.mvOpenDate = mvOpenDate;
 		this.mvNation = mvNation;
-		this.likeCount = likeCount;
-		this.mvName = mvName;
-		this.rewiewCount = rewiewCount;
+		this.orderBy = orderBy;
 	}
+
 
 	public String getCurrentScreening() {
 		return currentScreening;
 	}
 
+
 	public void setCurrentScreening(String currentScreening) {
 		this.currentScreening = currentScreening;
 	}
+
 
 	public String getCategoryName() {
 		return categoryName;
 	}
 
+
 	public void setCategoryName(String categoryName) {
 		this.categoryName = categoryName;
 	}
+
 
 	public String getViewRatiog() {
 		return viewRatiog;
 	}
 
+
 	public void setViewRatiog(String viewRatiog) {
 		this.viewRatiog = viewRatiog;
 	}
+
 
 	public String getMvOpenDate() {
 		return mvOpenDate;
 	}
 
+
 	public void setMvOpenDate(String mvOpenDate) {
 		this.mvOpenDate = mvOpenDate;
 	}
+
 
 	public String getMvNation() {
 		return mvNation;
 	}
 
+
 	public void setMvNation(String mvNation) {
 		this.mvNation = mvNation;
 	}
 
-	public int getLikeCount() {
-		return likeCount;
+
+	public String getOrderBy() {
+		return orderBy;
 	}
 
-	public void setLikeCount(int likeCount) {
-		this.likeCount = likeCount;
+
+	public void setOrderBy(String orderBy) {
+		this.orderBy = orderBy;
 	}
 
-	public String getMvName() {
-		return mvName;
-	}
-
-	public void setMvName(String mvName) {
-		this.mvName = mvName;
-	}
-
-	public int getRewiewCount() {
-		return rewiewCount;
-	}
-
-	public void setRewiewCount(int rewiewCount) {
-		this.rewiewCount = rewiewCount;
-	}
 
 	@Override
 	public String toString() {
 		return "SearchFilter [currentScreening=" + currentScreening + ", categoryName=" + categoryName + ", viewRatiog="
-				+ viewRatiog + ", mvOpenDate=" + mvOpenDate + ", mvNation=" + mvNation + ", likeCount=" + likeCount
-				+ ", mvName=" + mvName + ", rewiewCount=" + rewiewCount + "]";
+				+ viewRatiog + ", mvOpenDate=" + mvOpenDate + ", mvNation=" + mvNation + ", orderBy=" + orderBy + "]";
 	}
+
+	
 	
 	
 	
