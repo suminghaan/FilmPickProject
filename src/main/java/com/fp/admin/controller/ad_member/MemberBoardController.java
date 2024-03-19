1122f01f4fc9862d8d2cd42ca75f4f7088fe4122
recordmystory <113417749+recordmystory@users.noreply.github.com>
2024-03-19 17:59:54 +0900

    add : 회원 게시글 조회

---
+++ src/main/java/com/fp/admin/controller/ad_member/MemberBoardController.java
@@ -0,0 +1,87 @@
+package com.fp.admin.controller.ad_member;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fp.admin.model.service.MemberService;
+import com.fp.board.model.vo.Board;
+import com.fp.common.model.vo.PageInfo;
+
+/**
+ * Servlet implementation class MemberBoardListController
+ */
+@WebServlet("/list.bo")
+public class MemberBoardController extends HttpServlet { // 단순히 화면만 띄워주는 컨트롤러
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MemberBoardController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		 String memId = request.getParameter("memId");
+
+		int listCount;
+		int currentPage;
+		int pageLimit;
+		int boardLimit;
+		int maxPage;
+		int startPage;
+		int endPage;
+
+		listCount = new MemberService().selectBoardListCount(memId);
+
+		currentPage = Integer.parseInt(request.getParameter("page"));
+
+		pageLimit = 5;
+
+		boardLimit = 10;
+
+		maxPage = (int) Math.ceil((double) listCount / boardLimit);
+
+		startPage = (currentPage - 1) / pageLimit * pageLimit + 1;
+
+		endPage = startPage + pageLimit - 1;
+
+		if (endPage > maxPage) {
+			endPage = maxPage;
+		}
+
+		PageInfo pi = new PageInfo(listCount, currentPage, pageLimit, boardLimit, maxPage, startPage, endPage);
+
+		List<Board> pageList = new MemberService().selectBoardList(memId, pi);
+
+		List<Board> userProfile = new MemberService().selectUserBoardList(memId);
+		
+		request.setAttribute("pi", pi);
+		request.setAttribute("pageList", pageList);
+
+		request.getRequestDispatcher("/views/admin/ad_member/memberBoardPostView.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
