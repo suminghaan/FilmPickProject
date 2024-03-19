9ee14265ed72cf979d7c5db38e0c1d19678ee505
JE0NGEUNJE0NG <153482020+JE0NGEUNJE0NG@users.noreply.github.com>
2024-03-19 16:44:02 +0900

    update: MyPageController
    author: JE0NGEUNJE0NG
    
    add MyPageController
    - 선호장르 부분 수정 필요

---
+++ src/main/java/com/fp/member/controller/MemberMyPageController.java
@@ -0,0 +1,44 @@
+package com.fp.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MemberMyPageController
+ */
+@WebServlet("/myPage.me")
+public class MemberMyPageController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MemberMyPageController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// 응답페이지 : /views/mypage/myPage.jsp
+		// 응답데이터 : 현재 로그인한 회원의 정보
+		
+		request.getRequestDispatcher("/views/mypage/myPage.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
