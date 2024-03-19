255cb5c195d358e2c793b73ce87e7ea7c2ecd435
suminghaan <sm3597@daum.net>
2024-03-19 12:31:49 +0900

    docs sumin
    add 관리자 로그아웃 서블릿
    add 관리자 회원가입 아이디 중복확인 ajax

---
+++ src/main/java/com/fp/admin/controller/ad_resources/AjaxIdCheckController.java
@@ -0,0 +1,47 @@
+package com.fp.admin.controller.ad_resources;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fp.admin.model.service.AdminService;
+
+/**
+ * Servlet implementation class AjaxIdCheckController
+ */
+@WebServlet("/idCheck.ad")
+public class AjaxIdCheckController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjaxIdCheckController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String checkId = request.getParameter("checkId");
+		
+		int count = new AdminService().idCheck(checkId);
+		
+		response.getWriter().print(count > 0 ? "NNNNN" : "NNNNY");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
