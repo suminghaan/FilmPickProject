56ffa63abe0b912b0be37222479c1832713347c9
leeyonghum <yonghun92@naver.com>
2024-03-21 17:50:00 +0900

    20240321 서비스,dao 커밋
    커밋 충돌 해결용

--- src/main/java/com/fp/admin/model/dao/MovieDao.java
+++ src/main/java/com/fp/admin/model/dao/MovieDao.java
@@ -10,43 +10,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
-import com.fp.movie.model.vo.Movie;
-
 public class MovieDao {
 	
 	private Properties prop = new Properties();
 
-	// 메인페이지 영화 조회 구문 [용훈]
-	/*
-	public List<Movie> selectMainList(Connection conn) {
-		List<Movie> list = new ArrayList<>();
-		PreparedStatement pstmt = null;
-		ResultSet rset = null;
-		
-		String sql = prop.getProperty("selectMainList");
-		
-		try {
-			pstmt = conn.prepareStatement(sql);
-			rset = pstmt.executeQuery();
-			
-			while(rset.next()) {
-				list.add(new Movie(rset.getInt("MV_NO")
-									, rset.getString("MV_NAME")
-									, rset.getString("MV_OPENDATE")
-									, rset.getString("MV_STORY")
-									, rset.getString("FPAGE_EXPOSED")
-									, rset.getString("MPAGE_EXPOSED")
-									, rset.getString("MV_POSTER")
-									, rset.getString("MV_PREVIEW")
-									, rset.getString("MV_STATUS")));			
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}finally {
-			close(rset);
-			close(pstmt);
-		}
-		return list;
-	}
-	*/
+	
 }
