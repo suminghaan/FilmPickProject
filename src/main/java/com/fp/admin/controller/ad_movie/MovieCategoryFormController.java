830d2ce623c706fd9a5d495465bd508ccd8bd3fd
suminghaan <153481563+suminghaan@users.noreply.github.com>
2024-03-23 22:18:34 +0900

    docs movieCategory sumin
    add 영화카테고리 페이지 열어주는 서블릿, 헤더에 링크 추가

---
+++ src/main/java/com/fp/admin/controller/ad_movie/MovieCategoryFormController.java
@@ -0,0 +1,41 @@
+package com.fp.admin.controller.ad_movie;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MovieCategoryFormController
+ */
+@WebServlet("/movieCategoryForm.admo")
+public class MovieCategoryFormController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MovieCategoryFormController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getRequestDispatcher("/views/admin/ad_movie/movieCategory.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
