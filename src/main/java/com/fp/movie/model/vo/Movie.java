75e98fbc7a37934bc970aced113798bc0cd16833
suminghaan <153481563+suminghaan@users.noreply.github.com>
2024-03-23 22:03:49 +0900

    add movie.vo sumin
    add movie.java 영화상세조회시 필요한 필드 추가

--- src/main/java/com/fp/movie/model/vo/Movie.java
+++ src/main/java/com/fp/movie/model/vo/Movie.java
@@ -24,6 +24,13 @@ public class Movie {
 	// ** 2024.03.23 (관리자 : 한수민) 전체 영화조회시 필요한 필드(영화등록일, 영화에대한 카테고리) 추가
 	private String mvRegistDate;
 	private String categoryNames;	
+	// ** 2024.03.23 (관리자 : 한수민) 영화상세조회시 필요한 필드(배역, 등록요청한사용자, 등록요청자요청사항, 관리자번호 카테고리) 추가
+	private String casting;
+	private String nmUserRequest;
+	private String memNo;
+	private String adminNo;
+	
+	
 	
 	
 	public Movie() {
@@ -52,6 +59,51 @@ public class Movie {
 		this.categoryNames = categoryNames;
 	}
 
+	/**
+	 * (관리자) 영화 상세 조회 
+	 * @param mvNo
+	 * @param mvName
+	 * @param mvOpenDate
+	 * @param mvNation
+	 * @param mvRTime
+	 * @param mvStory
+	 * @param fPageExposed
+	 * @param viewRating
+	 * @param currentScreening
+	 * @param mvPoster
+	 * @param mvPreview
+	 * @param starRatingAvg
+	 * @param numberOfStarRating
+	 * @param categoryNames
+	 * @param casting
+	 * @param nmUserRequest
+	 * @param memNo
+	 * @param adminNo
+	 * 
+	 * @author 수민
+	 */
+	public Movie(int mvNo, String mvName, String mvOpenDate, String mvNation, String mvRTime, String mvStory,
+			String fPageExposed, String viewRating, String currentScreening, String mvPoster,
+			String mvPreview, String categoryNames, String casting, String nmUserRequest, String memNo,
+			String adminNo) {
+		super();
+		this.mvNo = mvNo;
+		this.mvName = mvName;
+		this.mvOpenDate = mvOpenDate;
+		this.mvNation = mvNation;
+		this.mvRTime = mvRTime;
+		this.mvStory = mvStory;
+		this.fPageExposed = fPageExposed;
+		this.viewRating = viewRating;
+		this.currentScreening = currentScreening;
+		this.mvPoster = mvPoster;
+		this.mvPreview = mvPreview;
+		this.categoryNames = categoryNames;
+		this.casting = casting;
+		this.nmUserRequest = nmUserRequest;
+		this.memNo = memNo;
+		this.adminNo = adminNo;
+	}
 
 
 	public Movie(int mvNo, String mvName, String mvOpenDate, String mvNation, String mvRTime, String mvStory,
@@ -266,6 +318,48 @@ public class Movie {
 	public void setCategoryNames(String categoryNames) {
 		this.categoryNames = categoryNames;
 	}
+	
+	
+
+	public String getCasting() {
+		return casting;
+	}
+
+
+	public void setCasting(String casting) {
+		this.casting = casting;
+	}
+
+
+	public String getNmUserRequest() {
+		return nmUserRequest;
+	}
+
+
+	public void setNmUserRequest(String nmUserRequest) {
+		this.nmUserRequest = nmUserRequest;
+	}
+
+
+	public String getMemNo() {
+		return memNo;
+	}
+
+
+	public void setMemNo(String memNo) {
+		this.memNo = memNo;
+	}
+
+
+	public String getAdminNo() {
+		return adminNo;
+	}
+
+
+	public void setAdminNo(String adminNo) {
+		this.adminNo = adminNo;
+	}
+
 
 	@Override
 	public String toString() {
