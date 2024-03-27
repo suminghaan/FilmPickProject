3ac836ae9338f37d3a55426551a4c3978a0b0c19
BaeKiWoong <12171790@inha.edu>
2024-03-27 15:44:46 +0900

    feat: 영화 장르 조회 기능 추가

--- src/main/java/com/fp/movie/model/dao/MovieDao.java
+++ src/main/java/com/fp/movie/model/dao/MovieDao.java
@@ -17,6 +17,7 @@ import com.fp.common.model.vo.Approval;
 import com.fp.common.model.vo.Attachment;
 import com.fp.common.model.vo.PageInfo;
 import com.fp.member.model.vo.Member;
+import com.fp.movie.model.vo.Category;
 import com.fp.movie.model.vo.Movie;
 import com.fp.movie.model.vo.Review;
 import com.fp.movie.model.vo.SearchFilter;
@@ -227,7 +228,7 @@ public class MovieDao {
 				m.setMvPoster(rset.getString("MV_POSTER"));
 				m.setMvPreview(rset.getString("MV_PREVIEW"));
 				m.setStarRatingAvg(rset.getString("AVG_STAR_RATING"));
-				m.setNumberOfStarRating(rset.getInt("NUMBER_OF_STAR_RATING"));
+				m.setNumberOfStarRating(rset.getString("NUMBER_OF_STAR_RATING"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -238,6 +239,9 @@ public class MovieDao {
 		
 		return m;
 	}
+	
+	// 영화 상세보기에서 영화 장르를 받아오는 메소드
+	
 
 	// 영화 상세보기 페이지에서 영화 추가사진 불러오는 메소드
 	public ArrayList<Attachment> selectAddiMovie(Connection conn, int movieNo) {
@@ -870,6 +874,36 @@ public class MovieDao {
 		}
 		
 		return result;
+	}
+
+	// 영화의 장르 정보를 가져오는 메소드 [기웅]
+	public ArrayList<Category> selectCategoryList(Connection conn, int movieNo) {
+		String query = prop.getProperty("selectCategoryList");
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		ArrayList<Category> categoryList = new ArrayList<>();
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, movieNo);
+			
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				Category category = new Category();
+				category.setCategoryNo(rset.getInt("CATEGORY_NO"));
+				category.setCategoryName(rset.getString("CATEGORY_NAME"));
+				
+				categoryList.add(category);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return categoryList;
 	}
 	
 
